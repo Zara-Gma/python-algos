912b7c630b7178edd44cbeedc3f3830f2939d374
Zara-Gma <43872052+Zara-Gma@users.noreply.github.com>
2021-04-12 16:46:27 -0700

    1108

--- Easy/Arrays/1480-1480-runningSum.py
+++ Easy/Arrays/1480-1480-runningSum.py
@@ -7,25 +7,10 @@
 # Output: [1,3,6,10]
 # Explanation: Running sum is obtained as follows: [1, 1+2, 1+2+3, 1+2+3+4].
 
-#Solution1
+#Solution
 class Solution(object):
     def runningSum(self, nums):
         for i in range(len(nums) -1):
             nums[i+1] += nums[i]
         return nums
-
-#Solution2
-class Solution(object):
-    def runningSum(self, nums):
-        runningSum = []
-        for num in nums:
-            runningSum.append(sum(nums[:len(runningSum)+1]))
-    return runningSum
-
-#Doesn't work
-# class Solution(object):
-#     def runningSum(self, nums):
-#         for i in range(len(nums+1)):
-#             nums[i] = nums[i] + nums[i]
-#         return nums
 # https://leetcode.com/problems/running-sum-of-1d-array
