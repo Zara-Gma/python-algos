09a418cd17f77b5bad705152d8a657d4a35bda85
Zara-Gma <43872052+Zara-Gma@users.noreply.github.com>
2021-04-13 14:57:23 -0700

    arrays

--- Easy/Arrays/Defanging IP Address.py
+++ Easy/Arrays/Defanging IP Address.py
@@ -4,12 +4,31 @@
  
 
 # Example 1:
-
+#                    |
 # Input: address = "1.1.1.1"
+# newaddress = "1[.]"
 # Output: "1[.]1[.]1[.]1"
 
+#Solution 1
+class Solution(object):
+    def defangIPaddr(self, address):
+        # for i in range(len(address)):
+
+        result = []
+        for character in address:
+            if character == '.':
+                result += list("[.]")
+            else:
+                result.append(character)
+            print(result)
+        return "".join(result)
+
+sol = Solution()
+print(sol.defangIPaddr("1.1.1.1"))
+
+
+#Solution 2
 class Solution(object):
     def defangIPaddr(self, address):
         return address.replace(".", "[.]")
-
 # https://leetcode.com/problems/richest-customer-wealth
--- Easy/Arrays/Running Sum.py
+++ Easy/Arrays/Running Sum.py
@@ -1,16 +1,25 @@
-#1480. Given an array nums. We define a running sum of an array as runningSum[i] = sum(nums[0]…nums[i]).
+#1480. Given an array nums. We define a running sum of an array as 
+# runningSum[i] = sum(nums[0]…nums[i]).
 # Return the running sum of nums.
 
-# Example 1:
-                 
+# Example 1:  
 # Input: nums = [1,2,3,4]
 # Output: [1,3,6,10]
 # Explanation: Running sum is obtained as follows: [1, 1+2, 1+2+3, 1+2+3+4].
+"""
+# Example 1:  
+    Index:       0 1 2 3 4
+# Input: nums = [1,2,3,4]
+"""
 
 #Solution
 class Solution(object):
     def runningSum(self, nums):
-        for i in range(len(nums) -1):
+        for i in range(len(nums)-1):
+            print('my index', i)
             nums[i+1] += nums[i]
         return nums
 # https://leetcode.com/problems/running-sum-of-1d-array
+
+sol = Solution()
+print(sol.runningSum([1, 2, 3, 4]))
