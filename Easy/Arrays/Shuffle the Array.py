3b663c6da1c28e8f0620b85d22efae359b5ceb40
Zara-Gma <43872052+Zara-Gma@users.noreply.github.com>
2021-05-12 11:13:35 -0700

    shuffle-the-array

--- Easy/Arrays/Shuffle the Array.py
+++ Easy/Arrays/Shuffle the Array.py
@@ -6,7 +6,15 @@
 # Input: nums = [2,5,1,3,4,7], n = 3
 # Output: [2,3,5,4,1,7] 
 # Explanation: Since x1=2, x2=5, x3=1, y1=3, y2=4, y3=7 then the answer is [2,3,5,4,1,7].
-
+        list = []
+        for k in range(len(nums)):
+            index = k
+            if k % 2 == 0:
+                index = k / 2
+            else:
+                index = (k / 2) + n 
+            list.append(nums[index])
+        return list
 
 
 # https://leetcode.com/problems/shuffle-the-array
